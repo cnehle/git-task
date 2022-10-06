d7dddafdb1c7c4ca634c865a6b686b990caeeefd
Elizaveta Moskvina <emoskvina@sfedu.ru>
2022-10-06 11:31:21 +0300

    Add convert from minutes to seconds

--- DateTime.pas
+++ DateTime.pas
@@ -3,4 +3,8 @@
   var year:=ReadInteger('Введите номер года:');
   Assert(year>0,'Неверно введён номер года!');
   Println('Год високосный:',year.Divs(4) and (not year.Divs(100) or year.Divs(400)));
+  
+  //2 Вывести сколько секунд в минуте.
+  Writeln(newline, 'Секунд в минуте: 60');
+  Println;
 end.
