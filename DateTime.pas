45041a8dcdc56b24f08aed515fdab7d5ae754bf1
Elizaveta Moskvina <emoskvina@sfedu.ru>
2022-10-06 11:29:15 +0300

    Add is leap year program

---
+++ DateTime.pas
@@ -0,0 +1,6 @@
+﻿begin
+  //1 Проведить год на високосность.
+  var year:=ReadInteger('Введите номер года:');
+  Assert(year>0,'Неверно введён номер года!');
+  Println('Год високосный:',year.Divs(4) and (not year.Divs(100) or year.Divs(400)));
+end.
